d72d25e3ba2f77cf1026468e62710395eee95b1c
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-18 21:42:56 +0100

    Project-1 || Update Nav bar

--- Project-1 React Info Site/Project-1/src/components/Navbar.jsx
+++ Project-1 React Info Site/Project-1/src/components/Navbar.jsx
@@ -2,12 +2,8 @@ import logo from "../assets/reactjs-icon.png";
 export default function Navbar() {
 	return (
 		<nav>
-			<div className="logo">
-				<div className="nav">
-					<img src={logo} alt="" />
-					<h2>ReactFacts</h2>
-				</div>
-			</div>
+			<img src={logo} alt="" />
+			<h2>ReactFacts</h2>
 			<h2>React Course - Project 1</h2>
 		</nav>
 	);
