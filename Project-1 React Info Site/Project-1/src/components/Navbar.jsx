661120036d6ae77f5be7829be42e53ea5af711e5
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-18 21:25:25 +0100

    Project-1 || Add Nav bar + its styles

--- Project-1 React Info Site/Project-1/src/components/Navbar.jsx
+++ Project-1 React Info Site/Project-1/src/components/Navbar.jsx
@@ -1,3 +1,14 @@
+import logo from "../assets/reactjs-icon.png";
 export default function Navbar() {
-	return <h1>Navbar goes here</h1>;
+	return (
+		<nav>
+			<div className="logo">
+				<div className="nav">
+					<img src={logo} alt="" />
+					<h2>ReactFacts</h2>
+				</div>
+			</div>
+			<h2>React Course - Project 1</h2>
+		</nav>
+	);
 }
