8162cae52969a5f67eab9f6b3707d0da51e8e315
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-19 21:38:22 +0100

    Project-2 || Add Hero

--- Projects/Project-2/src/components/Hero.jsx
+++ Projects/Project-2/src/components/Hero.jsx
@@ -1,3 +1,15 @@
+import Bg from "../assets/Group 77.png";
 export default function Hero() {
-	return <div className="hero">Hero goes here</div>;
+	return (
+		<div className="hero">
+			<div className="img">
+				<img src={Bg} alt="" />
+			</div>
+			<h1>Online Experiences</h1>
+			<p>
+				Join unique interactive activities led by one-of-a-kind hosts—all
+				without leaving home.
+			</p>
+		</div>
+	);
 }
