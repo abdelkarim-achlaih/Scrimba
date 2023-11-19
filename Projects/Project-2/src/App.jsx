32ce88217b9dc2afe2c969a9ff606e91731e5e90
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-11-19 21:18:53 +0100

    Project-2 || Add Navbar

--- Projects/Project-2/src/App.jsx
+++ Projects/Project-2/src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar";
-import Main from "./components/Main";
+import Hero from "./components/Hero";
 
 export default function App() {
 	return (
 		<>
 			<Navbar />
-			<Main />
+			<Hero />
 		</>
 	);
 }
