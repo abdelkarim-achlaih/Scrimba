c3e1fb071ce317f6760b4c5c05f72e1501c1a713
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-01-08 15:33:34 +0100

    Meme Compo inside main

--- Projects/Project-3/src/components/Meme.jsx
+++ Projects/Project-3/src/components/Meme.jsx
@@ -1,22 +1,26 @@
+import memesData from "../memesData";
+
 export default function Meme() {
 	return (
-		<form action="" className="meme">
-			<div className="inputs">
-				<div className="input-field">
-					<label htmlFor="top">Top text</label>
-					<input type="text" name="top" id="top" placeholder="Shut up" />
+		<main>
+			<div action="" className="meme">
+				<div className="inputs">
+					<div className="input-field">
+						<label htmlFor="top">Top text</label>
+						<input type="text" name="top" id="top" placeholder="Shut up" />
+					</div>
+					<div className="input-field">
+						<label htmlFor="bottom">Bottom text</label>
+						<input
+							type="text"
+							name="bottom"
+							id="bottom"
+							placeholder="And take my money"
+						/>
+					</div>
 				</div>
-				<div className="input-field">
-					<label htmlFor="bottom">Bottom text</label>
-					<input
-						type="text"
-						name="bottom"
-						id="bottom"
-						placeholder="And take my money"
-					/>
-				</div>
+				<input type="submit" value="Get a new meme image  🖼" />
 			</div>
-			<input type="submit" value="Get a new meme image  🖼" />
-		</form>
+		</main>
 	);
 }
