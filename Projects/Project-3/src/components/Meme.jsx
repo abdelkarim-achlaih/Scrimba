aab4df9d5f8191438187b066dcef0f1e040ba410
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-01-08 09:30:07 +0100

    Adding Meme Compo

---
+++ Projects/Project-3/src/components/Meme.jsx
@@ -0,0 +1,22 @@
+export default function Meme() {
+	return (
+		<form action="" className="meme">
+			<div className="inputs">
+				<div className="input-field">
+					<label htmlFor="top">Top text</label>
+					<input type="text" name="top" id="top" placeholder="Shut up" />
+				</div>
+				<div className="input-field">
+					<label htmlFor="bottom">Bottom text</label>
+					<input
+						type="text"
+						name="bottom"
+						id="bottom"
+						placeholder="And take my money"
+					/>
+				</div>
+			</div>
+			<input type="submit" value="Get a new meme image  🖼" />
+		</form>
+	);
+}
