7bc5875399786e1f32c4e46906dd24a4c49ce1cf
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-01-20 21:55:43 +0100

    Adding Firebase config file

---
+++ Projects/Project-4/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDRM7ox3PrUJPbBL31yQM4OONIbXUnRmwc",
+	authDomain: "react-notes-9c5bb.firebaseapp.com",
+	projectId: "react-notes-9c5bb",
+	storageBucket: "react-notes-9c5bb.appspot.com",
+	messagingSenderId: "447664319661",
+	appId: "1:447664319661:web:ded1bc29766fbb20c4aa24",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
