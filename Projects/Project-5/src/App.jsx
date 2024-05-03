8b453b5ac05461bd09ec2b3557d51de0d121a5b4
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-03 22:01:51 +0100

    Add Game Structure + Styles

--- Projects/Project-5/src/App.jsx
+++ Projects/Project-5/src/App.jsx
@@ -1,7 +1,36 @@
 import { useState } from "react";
 
 function App() {
-	return <main></main>;
+	return (
+		<main>
+			<div className="container">
+				<div className="copy">
+					<h1>Tenzies</h1>
+					<p>
+						Roll until all dice are the same. Click each die to freeze it at its
+						current value between rolls.
+					</p>
+				</div>
+				<div className="game">
+					<ul>
+						<li className="number active">5</li>
+						<li className="number">5</li>
+						<li className="number">5</li>
+						<li className="number active">5</li>
+						<li className="number">5</li>
+					</ul>
+					<ul>
+						<li className="number">6</li>
+						<li className="number">6</li>
+						<li className="number">6</li>
+						<li className="number active">6</li>
+						<li className="number">6</li>
+					</ul>
+					<button>Roll</button>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default App;
