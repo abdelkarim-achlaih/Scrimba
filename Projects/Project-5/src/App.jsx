6968684ba81393b401a5e8a148b692438f6dded6
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-06 02:09:34 +0100

    End Game Logic

--- Projects/Project-5/src/App.jsx
+++ Projects/Project-5/src/App.jsx
@@ -1,11 +1,28 @@
 import Die from "./components/Die";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
 function App() {
 	const [dice, setDice] = useState(() => {
 		return allNewDice();
 	});
+
+	const [tenzies, setTenzies] = useState(false);
+
+	useEffect(() => {
+		// Using Array.prototype.reduce()
+
+		// const winRuleOne = dice.reduce((final, die) => die.isHeld * final, true);
+		// const winRuleTwo =
+		// 	dice[0].value ** 10 === dice.reduce((final, die) => die.value * final, 1);
+
+		// Using Array.prototype.every()
+		const winRuleOne = dice.every((die) => die.isHeld); //Returns bool based on a condition was verified by all elements of the array or not
+		const winRuleTwo = dice.every((die) => die.value === dice[0].value);
+		if (winRuleOne && winRuleTwo) {
+			console.log("You won!");
+		}
+	}, [dice]);
 
 	const diceToRender = dice.map((die, i) => (
 		<Die
@@ -17,6 +34,7 @@ function App() {
 			}}
 		/>
 	));
+
 	function generateNewDie() {
 		const max = 6;
 		const min = 1;
@@ -26,6 +44,7 @@ function App() {
 			id: nanoid(),
 		};
 	}
+
 	function allNewDice() {
 		let numbers = [];
 		const arrayLength = 10;
@@ -34,11 +53,13 @@ function App() {
 		}
 		return numbers;
 	}
+
 	function rollDice() {
 		setDice((prevDice) =>
 			prevDice.map((die) => (!die.isHeld ? generateNewDie() : die))
 		);
 	}
+
 	function holdDice(id) {
 		setDice((prevDice) =>
 			prevDice.map((die) =>
