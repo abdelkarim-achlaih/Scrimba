18ccec95d1f7918eaced76cc696e2ae484b315a5
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-25 20:39:44 +0100

    Adding a headless toggle compo

--- Projects/Project-6/index.jsx
+++ Projects/Project-6/index.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Menu from "./components/Menu/index"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Menu from "./components/Menu/index";
+import Toggle from "./components/Toggle/index";
 
 function App() {
-  const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"]
+	const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
 
-  return (
-    <Menu>
-      <Menu.Button>Sports</Menu.Button>
-      <Menu.Dropdown>
-        {sports.map(sport => (
-          <Menu.Item key={sport}>{sport}</Menu.Item>
-        ))}
-      </Menu.Dropdown>
-    </Menu>
-  )
+	return (
+		<Toggle>
+			<Menu>
+				<Toggle.Button>
+					<Menu.Button>Menu</Menu.Button>
+				</Toggle.Button>
+				<Toggle.On>
+					<Menu.Dropdown>
+						<Menu.Item>Home</Menu.Item>
+						<Menu.Item>About</Menu.Item>
+						<Menu.Item>Contact</Menu.Item>
+						<Menu.Item>Blog</Menu.Item>
+					</Menu.Dropdown>
+				</Toggle.On>
+			</Menu>
+		</Toggle>
+	);
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
