78a4859c6a74255babba836cc58cd600e2afd765
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-27 19:19:40 +0100

    Add Display Component to Toggle: Expose internal state to give more control to user

--- Projects/Project-6/index.jsx
+++ Projects/Project-6/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Menu from "./components/Menu/index";
 import Star from "./components/Star";
+import Toggle from "./components/Toggle/index";
 
 function App() {
 	return (
@@ -18,6 +19,17 @@ function App() {
 					<Menu.Item>Blog</Menu.Item>
 				</Menu.Dropdown>
 			</Menu>
+			<br />
+
+			<Toggle>
+				<Toggle.Button>
+					<Toggle.Display>
+						{(on) => {
+							return <div className={`box ${on && "filled"}`}></div>;
+						}}
+					</Toggle.Display>
+				</Toggle.Button>
+			</Toggle>
 		</>
 	);
 }
