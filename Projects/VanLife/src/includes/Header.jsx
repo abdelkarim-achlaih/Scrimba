5b1d0a478bd87e39b593d53c91ab1af9512317a4
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-28 15:44:23 +0100

    Adding Some Content and styling

--- Projects/VanLife/src/includes/Header.jsx
+++ Projects/VanLife/src/includes/Header.jsx
@@ -3,15 +3,11 @@ import { Link } from "react-router-dom";
 export default function Header() {
 	return (
 		<header>
+			<Link className="site-logo" to="/">
+				#VanLife
+			</Link>
 			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/about">About</Link>
-					</li>
-				</ul>
+				<Link to="/about">About</Link>
 			</nav>
 		</header>
 	);
