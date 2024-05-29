c672f470dbd6b3f5a22df05d214d33f033b2a248
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-29 18:49:15 +0100

    Receive the get request and show van details

--- Projects/VanLife/src/pages/VanDetail.jsx
+++ Projects/VanLife/src/pages/VanDetail.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
+	const [van, setVan] = React.useState({});
+
+	const params = useParams();
+
+	React.useEffect(() => {
+		async function getData() {
+			const req = await fetch(`/api/vans/${params.id}`);
+			const response = await req.json();
+			setVan(response.vans);
+		}
+		getData();
+	}, []);
+
 	return (
-		// <div className="van-tile">
-		// 	<Link to={`/:${id}`}>
-		// 		<img src={imageUrl} />
-		// 		<div className="van-info">
-		// 			<h3>{name}</h3>
-		// 			<p>
-		// 				${price}
-		// 				<span>/day</span>
-		// 			</p>
-		// 		</div>
-		// 		<i className={`van-type ${type} selected`}>{type}</i>
-		// 	</Link>
-		// </div>
-		<h1>Van Detail Page</h1>
+		<div className="van-tile">
+			<img src={van.imageUrl} />
+			<div className="van-info">
+				<h3>{van.name}</h3>
+				<p>
+					${van.price}
+					<span>/day</span>
+				</p>
+			</div>
+			<i className={`van-type ${van.type} selected`}>{van.type}</i>
+		</div>
 	);
 }
