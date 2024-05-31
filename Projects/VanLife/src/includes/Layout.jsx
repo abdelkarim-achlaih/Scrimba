9905483238e52f32d7a5bd020041e386f9843b98
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-31 08:12:45 +0100

    Bootstrapping Host Pages

--- Projects/VanLife/src/includes/Layout.jsx
+++ Projects/VanLife/src/includes/Layout.jsx
@@ -3,9 +3,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 export default function Layout() {
-	/**
-	 * Challenge: get the Header working again
-	 */
 	return (
 		<>
 			<Header />
