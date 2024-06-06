e23d263310f39ea34db1b1d21e55c45567eddbd4
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-06-06 16:29:36 +0100

    Adding 404 page

--- Projects/VanLife/src/App.jsx
+++ Projects/VanLife/src/App.jsx
@@ -14,6 +14,7 @@ import HostVanDetail from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import NotFound from "./pages/NotFound";
 
 import "../server";
 
@@ -37,6 +38,7 @@ export default function App() {
 							<Route path="photos" element={<HostVanPhotos />} />
 						</Route>
 					</Route>
+					<Route path="*" element={<NotFound />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
