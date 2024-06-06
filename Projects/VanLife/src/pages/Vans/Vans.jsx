c161b76fa92eb6efe894f493c9e054970cfdb9f6
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-06-06 13:04:44 +0100

    Navigate to some query without ovveritting old key-value pairs using URLSearchParams Object constructor

--- Projects/VanLife/src/pages/Vans/Vans.jsx
+++ Projects/VanLife/src/pages/Vans/Vans.jsx
@@ -22,22 +22,58 @@ export default function Vans() {
 		: vans;
 
 	const vanElements = displayedVans.map((van) => <Van key={van.id} {...van} />);
+
+	function genNewSPString(key, value) {
+		const sp = new URLSearchParams(searchParams);
+		value === null ? sp.delete(key) : sp.set(key, value);
+		return `?${sp.toString()}`;
+	}
+
 	return (
 		<div className="van-list-container">
 			<h1>Explore our van options</h1>
 			<div className="van-list-filter-buttons">
-				<Link className="van-type simple" to="?type=simple">
+				<Link className="van-type simple" to={genNewSPString("type", "simple")}>
 					Simple
 				</Link>
-				<Link className="van-type luxury" to="?type=luxury">
+				<Link className="van-type luxury" to={genNewSPString("type", "luxury")}>
 					Luxury
 				</Link>
-				<Link className="van-type rugged" to="?type=rugged">
+				<Link className="van-type rugged" to={genNewSPString("type", "rugged")}>
 					Rugged
 				</Link>
-				<Link className="van-type clear-filters" to=".">
+				<Link
+					className="van-type clear-filters"
+					to={genNewSPString("type", null)}
+				>
 					Clear
 				</Link>
+				{/* <button
+					className="van-type luxury"
+					onClick={() => setSearchParams({ type: "simple" })}
+				>
+					Simple
+				</button>
+				<button
+					className="van-type luxury"
+					onClick={() => setSearchParams({ type: "luxury" })}
+				>
+					Luxury
+				</button>
+				<button
+					className="van-type rugged"
+					onClick={() => setSearchParams({ type: "rugged" })}
+				>
+					Rugged
+				</button>
+				{typeFilter && (
+					<button
+						className="van-type clear-filters"
+						onClick={() => setSearchParams({})}
+					>
+						Clear
+					</button>
+				)} */}
 			</div>
 			<div className="van-list">{vanElements}</div>
 		</div>
